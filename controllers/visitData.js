52a5f1e9d9f2e6d2e52819e31814ca71485e96ea
Rohit Vishwakarma <rohit.vishwakarma@logicoy.com>
2024-05-30 23:15:41 +0530

    date fix

--- controllers/visitData.js
+++ controllers/visitData.js
@@ -2,7 +2,7 @@ const Visits = require('../models/visits')
 module.exports.getMedicalBill = async function(req, res){
     try{
         let visit = await Visits.findById(req.params.visitId).populate('PatientId');
-        return res.render('medbill',{patient:visit.PatientId, prescriptions:visit.Prescriptions,role:req.user.role});
+        return res.render('medbill',{patient:visit.PatientId, prescriptions:visit.Prescriptions,role:req.user.role, visitDate:visit.createdAt});
     }catch(err){
         console.log(err)
         return res.redirect('back')
@@ -15,7 +15,7 @@ module.exports.oldMedBill = async function(req, res){
         let d = req.query.date.split('-');
         let modifiedDate =  d[0]+'-'+String(Number(d[1]))+'-'+String(Number(d[2]));
         let visit = await Visits.findOne({PatientId:req.params.patientId, Date:modifiedDate}).populate('PatientId');
-        return res.render('medbill_old', {patient:visit.PatientId, prescriptions:visit.Prescriptions,role:req.user.role});
+        return res.render('medbill_old', {patient:visit.PatientId, prescriptions:visit.Prescriptions,role:req.user.role,visitDate:visit.createdAt});
     }catch(err){
         console.log(err);
         return res.redirect('back')
